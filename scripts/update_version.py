bc53a9079999e6b631336fd54c0837cf9441f04e
JPK85 <52880112+JPK85@users.noreply.github.com>
2025-08-24 20:27:53 +0300

    Formatted version script

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,28 +1,30 @@
 from pathlib import Path
+
 import toml
 
+
 def update_version_in_init():
-    pyproject_path = Path(__file__).parent.parent / 'pyproject.toml'
+    pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
     if not pyproject_path.exists():
-        print(f'pyproject.toml not found at {pyproject_path}')
+        print(f"pyproject.toml not found at {pyproject_path}")
         return
 
     pyproject = toml.load(pyproject_path)
-    version = pyproject['tool']['poetry']['version']
+    version = pyproject["tool"]["poetry"]["version"]
 
     init_file_path = (
-        Path(__file__).parent.parent / 'src' / 'patentpack' / '__init__.py'
+        Path(__file__).parent.parent / "src" / "patentpack" / "__init__.py"
     )
     if not init_file_path.exists():
-        print(f'__init__.py not found at {init_file_path}')
+        print(f"__init__.py not found at {init_file_path}")
         return
 
     init_file_content = init_file_path.read_text()
-    new_init_file_content = ''
+    new_init_file_content = ""
     version_found = False
 
-    for line in init_file_content.split('\n'):
-        if line.startswith('__version__'):
+    for line in init_file_content.split("\n"):
+        if line.startswith("__version__"):
             new_init_file_content += f"__version__ = '{version}'\n"
             version_found = True
         else:
@@ -32,7 +34,8 @@ def update_version_in_init():
         new_init_file_content += f"__version__ = '{version}'\n"
 
     init_file_path.write_text(new_init_file_content.strip())
-    print(f'Updated __version__ in __init__.py to {version}')
+    print(f"Updated __version__ in __init__.py to {version}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     update_version_in_init()
